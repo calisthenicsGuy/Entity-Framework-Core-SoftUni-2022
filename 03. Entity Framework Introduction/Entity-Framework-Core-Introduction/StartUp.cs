ae5ea1f5a54dff35e1ae00e5b4c4a0bfbb1eb35f
calisthenicsGuy <radevradoslav12@gmail.com>
2022-07-12 19:36:10 +0300

    Entity Framework Introduction Demo Added

---
+++ 03. Entity Framework Introduction/Entity-Framework-Core-Introduction/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Entity_Framework_Core_Introduction.Models;
+using System.Collections;
+
+namespace Entity_Framework_Core_Introduction
+{
+    public class StartUp
+    {
+        static async Task Main(string[] args)
+        {
+            using SoftUniContext dbContext = new SoftUniContext();
+
+            Employee firstEmployee = await dbContext.Employees.FindAsync(5);
+
+            var secondEmployee = await dbContext.Employees
+                .Include(a => a.Address)
+                .ThenInclude(t => t.Town)
+                .FirstOrDefaultAsync();
+            ;
+        }
+    }
+}
