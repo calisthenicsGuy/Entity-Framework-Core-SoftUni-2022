201f5e58e65550e3862b9493aab43538d2f5fd0b
calisthenicsGuy <radevradoslav12@gmail.com>
2022-07-20 20:03:56 +0300

    XML demos added

---
+++ 10. XML Processing/CreatingXMLFileWthLINQ/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace CreatingXMLFileWthLINQ
+{
+    [XmlType("book")]
+    public class Book
+    {
+
+        [XmlElement("title")]
+        public string Title { get; set; }
+
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+
+        [XmlIgnore]
+        public string Descroption { get; set; }
+    }
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           /*  XDocument document = new XDocument();
+            XElement root = new XElement("Library");
+            document.Add(root);
+
+            for (int i = 1; i <= 100; i++)
+            {
+                XElement book = new XElement("book");
+                root.Add(book);
+                book.Add(new XElement("title", $" #{i} "));
+                book.Add(new XElement("price", $"${i + 2.5}"));
+            }
+
+            document.Save(@"..\..\..\XMLData\Library.xml"); */
+
+            // Deserialize - Xml to ojects
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Book>), new XmlRootAttribute("library"));
+            List<Book> books1 = (List<Book>)serializer.Deserialize(File.OpenRead(@"..\..\..\XMLData\Library.xml"));
+
+
+            // Serialize - Ojects to Xml
+            List<Book> books2 = new List<Book>();
+            books2.Add(new Book() { Title = "Book1", Price = 20.00m });
+            books2.Add(new Book() { Title = "Book2", Price = 18.00m });
+            books2.Add(new Book() { Title = "Book3", Price = 22.00m });
+            serializer.Serialize(File.OpenWrite(@"..\..\..\XMLData\randomBooks.xml"), books2);
+
+        }
+    }
+}
---
+++ 10. XML Processing/XML-Demo01/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace XML_Demo01
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            // XDocument
+            string foodMenuFromXMLFile = File.ReadAllText(@"..\..\..\XMLData\FoodMenu.xml"); // read the text as string
+            XDocument document1 = XDocument.Parse(foodMenuFromXMLFile); // parse the particular string to xml document
+            XDocument document2 = XDocument.Load(@"..\..\..\XMLData\FoodMenu.xml"); // parse the text to xml file from file path
+
+
+            // Declaration -> Xml file declaration
+            string versionOfXmlFile = document1.Declaration.Version;
+            string encodingOfXmlFile = document1.Declaration.Encoding;
+
+            // Root - Linq to XML
+            int countOfRootElements = document1.Root.Elements().Count();
+            XElement firstElementThatHaveAttributeTest = document1.Root
+                .Elements()
+                .Where(x => x.Attributes().Any(a => a.Name == "test")).FirstOrDefault();
+
+            XElement firstElementThatHaveElementName = document1.Root.Elements()
+                .Where(e => e.Elements().Any(se => se.Name == "name"))
+                .FirstOrDefault();
+
+            int totalElementsInDocumnet = document1.Root.Elements().Elements().Count();
+
+            StringBuilder foodsName = new StringBuilder();
+            foreach (var food in document1.Root.Elements())
+            {
+                foodsName.AppendLine(food.Element("name").Value);
+            }
+
+            // Change the XML document ->
+            // SetElementValue, Remove, RemoveAll, Attribute, SetAttributeValue, RemoveAttributes
+            /* foreach (var food in document1.Root.Elements())
+            {
+                food.SetAttributeValue("food", food.Element("name").Value.ToString());
+            }
+            document1.Save(@"..\..\..\XMLData\FoodMenu_02.xml"); */
+
+            List<XElement> foodsThatHaveMoreThan800Calories = document1.Root.Elements()
+                .Where(x => int.Parse((string)x.Element("calories")) > 800)
+                .ToList();
+
+
+        }
+    }
+}
